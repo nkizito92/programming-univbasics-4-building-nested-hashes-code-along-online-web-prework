cbf36f9c93a00bd51989538db72ede24996cac35
Kizito Njoku <nkizito57@gmail.com>
2019-10-16 19:24:49 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,7 +2,9 @@ def first_challenge
    epic_tragedy = {
      :montague => {
        :patriarch =>{},
-       :
+       :matriarch => {},
+       :hero => {},
+       :hero_friends => []
      },
      :capulet => {}
    }
