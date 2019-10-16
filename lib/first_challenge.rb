1f409531250cd234f089445de91a8e6d63430a8e
Kizito Njoku <nkizito57@gmail.com>
2019-10-16 20:46:41 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,7 +1,10 @@
 def first_challenge
    epic_tragedy = {
      :montague => {
-       :patriarch => {:name => "Lord Montague",:age => "53"},
+       :patriarch => {
+         :name => "Lord Montague",
+         :age => "53"  
+       },
        :matriarch => {
          :name => "Lady Montague",
          :age => "54"
