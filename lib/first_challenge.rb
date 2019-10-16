e6b6b803ea7dbf0f1adfed176aa1f364f76d68d4
Kizito Njoku <nkizito57@gmail.com>
2019-10-16 20:27:54 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -6,15 +6,30 @@ def first_challenge
          :age => "53"  
        },
        :matriarch => {
-         :naem
+         :name => "Lady Montague",
+         :age => "54"
        },
-       :hero => {},
+       :hero => {
+         :name => "Romeo",
+         :age => "15",
+         :status => "alive"
+       },
        :hero_friends => []
      },
      :capulet => {
-      :patriarch => {},
-      :matriarch => {},
-      :heroine => {},
+      :patriarch => {
+        :name => "Lord Capulet",
+        :age => "50"
+      },
+      :matriarch => {
+        :name => "Lady Capulet",
+        :age => "51"
+      },
+      :heroine => {
+        :name => "Juliet",
+        :age => "15"
+        :status => "alive"
+      },
       :heroine_friends => []
      }
    }
